4818154f33d02bd7b51a0f0fd14b4f57d0df355d
Joshua Marketis <josh@clidus.com>
2014-08-15 08:42:10 +0100

    Increment version number to v0.2

--- ignition_application/controllers/auth.php
+++ ignition_application/controllers/auth.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Ignition v0.1 ignitionpowered.co.uk
+| Ignition v0.2 ignitionpowered.co.uk
 |--------------------------------------------------------------------------
 |
 | This class extends the functionality of Ignition. You can add your
