465c2daedde65c45c2596cd45ac9a5630c890115
Joshua Marketis <josh@clidus.com>
2015-01-17 19:11:24 +0000

    Rename Ignition css

--- ignition_application/views/templates/header.php
+++ ignition_application/views/templates/header.php
@@ -11,7 +11,7 @@
 
         <!-- CSS -->
         <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
-        <link href="/css/stylesheet.css" rel="stylesheet">
+        <link href="/css/ignition.css" rel="stylesheet">
     </head>
     <body>
       <!-- navbar -->
