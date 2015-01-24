a5237a03701c4efdf3c6f9de6e8bbb18c55e87a6
Joshua Marketis <josh@clidus.com>
2015-01-24 21:48:08 +0000

    Increment version number to v0.3.1

--- ignition_application/controllers/home.php
+++ ignition_application/controllers/home.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Ignition v0.3 ignitionpowered.co.uk
+| Ignition ignitionpowered.co.uk
 |--------------------------------------------------------------------------
 |
 | This class extends the functionality of Ignition. You can add your
