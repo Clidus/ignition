516fd887348431bc34da0a8fe424958822e0aea1
Josh Marketis <josh@clidus.com>
2015-07-29 22:05:40 +0100

    Provide examples in config

--- ignition_application/config/ignition_config.php
+++ ignition_application/config/ignition_config.php
@@ -1,10 +1,19 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/* Website name */
+/* 
+	Website name 
+	Example: Ignition
+*/
 $config['website_name'] = '';
 
-/* password reset email address */
+/* 
+	Password reset email address 
+	Example: password@ignitionpowered.co.uk
+*/
 $config['email_password_reset'] = '';
 
-/* default profile image (located in /uploads) */
+/* 
+	Default profile image file name (located in /uploads) 
+	Example: default.png
+*/
 $config['default_profile_image'] = 'default.png';
